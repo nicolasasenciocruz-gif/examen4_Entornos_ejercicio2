a9dc1f4731ee3459eeded964103280aeb717d06d
NicolasAsenciocruz <Canchispe@gmail.com>
2026-04-16 09:48:22 +0200

    First commit

---
+++ src/Empleado.java
@@ -0,0 +1,75 @@
+public class Empleado {
+    private String dni;
+    private String nombre;
+    private String direccion;
+    private String cargo;
+    private int numHijos;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getNumHijos() {
+        return numHijos;
+    }
+
+    public void setNumHijos(int numHijos) {
+        this.numHijos = numHijos;
+    }
+
+    public Empleado (String dni, String nombre){
+        this.dni=dni;
+        this.nombre=nombre;
+        this.cargo="EMPLEADO";
+        this.numHijos=0;
+    }
+
+    public double calcula_sueldo(double base){
+        double total=base;
+
+        if(cargo.equals("ENCARGADO"))
+            total+=300;
+        else if(cargo.equals("DIRECTOR"))
+            total+=1000;
+        if(numHijos>=3)
+            total+=numHijos*50;
+        return total;
+    }
+    public void infoEmpleado() {
+        System.out.println("DNI: " + dni);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Cargo: " + cargo);
+        System.out.println("Número de hijos: " + numHijos);
+        System.out.println("Sueldo: " + calcula_sueldo(1200));
+    }
+
+
+}
---
+++ src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args) {
+            Empleado empleado = new Empleado("44885522R", "Juan Gómez");
+            empleado.infoEmpleado();
+    }
+
+}
